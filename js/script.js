2af31b3f2ad0e0f42b909eeca64b4a1ddb03c150
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-29 18:27:27 +0200

    feat: stampa di 10 email random

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 const { createApp } = Vue;
 
-createApp ({
-    data (){
-        return{
-            
+createApp({
+    data() {
+        return {
+            emailArray: [],
         }
     },
-    created(){
+    created() {
         this.genRandomEmail();
     },
-    methods:{
-        genRandomEmail (){
-            axios.get ("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then ((resp) => {
-                console.log(resp.data.response);
-            })
+    methods: {
+        genRandomEmail() {
+            for (i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((resp) => {
+                        console.log(resp.data.response);
+                        this.emailArray.push(resp.data.response)
+                    })
+            }
         }
     }
 
